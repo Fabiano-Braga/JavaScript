b6957edbbca4bfaca2ee491892e6a9332d25e046
Fabiano-braga <fabiano.braga@a-technologies.com.br>
2022-06-15 02:22:31 -0300

    Verificando hora do dia

--- desafios/hora-do-dia/scripts/modelo.js
+++ desafios/hora-do-dia/scripts/modelo.js
@@ -1,5 +1,24 @@
-let data = new Date()
-let hora = data.getHours()
-let msg = document.querySelector('div#msg')
-let img = document.getElementById('imagem')
+function carregar() {
+    let data = new Date()
+    let hora = data.getHours()
+    let msg = document.getElementById('msg')
+    let img = document.getElementById('imagem')
+    msg.innerHTML = `Agora são ${hora} horas`
 
+    if (hora >=0 && hora < 12) {
+        img.src = 'imagens/manha.png'
+        document.body.style.background = '#375b73'
+
+    }else if (hora >= 12 && hora < 18) {
+        img.src = 'imagens/tarde.png'
+        document.body.style.background = '#cb8374'
+
+    }else {
+        img.src = 'imagens/noite.png'
+        document.body.style.background = '#525961'
+    }
+
+
+
+
+}
