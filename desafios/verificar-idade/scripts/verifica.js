91bce0a58aa65c4c79cbe8bfe1cc4abd1710a940
Fabiano-braga <fabiano.braga@a-technologies.com.br>
2022-06-19 18:13:40 -0300

    Update verifica.js

--- desafios/verificar-idade/scripts/verifica.js
+++ desafios/verificar-idade/scripts/verifica.js
@@ -5,10 +5,18 @@ function Verificar() {
     var res = document.getElementById('res')
     var idade = ano - nasc.value
     var sexo = document.getElementsByName('radiosex')
+    var genero = ''
    
     if (nasc.value.length == 0 || nasc.value > ano) {
         alert(`ERRO: Verifique os dados e tente novamente... `)
+    }else {
+        if (sexo[0].checked) {
+            genero = 'Homem'
+        }else {
+            genero = 'Mulher'
+        }
     }
+    res.innerHTML = `${genero} `
 
     
 }
