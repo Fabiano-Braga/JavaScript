4334ad9d98536feaef9727f1a08c23a385b934a3
Fabiano-braga <fabiano.braga@a-technologies.com.br>
2022-06-21 14:53:31 -0300

    Projeto finalizado

--- desafios/verificar-idade/scripts/verifica.js
+++ desafios/verificar-idade/scripts/verifica.js
@@ -6,17 +6,48 @@ function Verificar() {
     var idade = ano - nasc.value
     var sexo = document.getElementsByName('radiosex')
     var genero = ''
+    var img = document.createElement('img')
+    img.setAttribute('id', 'foto')
    
     if (nasc.value.length == 0 || nasc.value > ano) {
         alert(`ERRO: Verifique os dados e tente novamente... `)
+
     }else {
         if (sexo[0].checked) {
-            genero = 'Homem'
-        }else {
-            genero = 'Mulher'
-        }
-    }
-    res.innerHTML = `${genero} `
+            genero = 'Masculino'
+            if (idade >= 0 && idade <= 10) {
+                img.setAttribute('src', 'imagens/bebe-menino.png')
 
+            }else if (idade <= 21) {
+                img.setAttribute('src', 'imagens/jovem-homem.png')
+
+            }else if (idade <= 50) {
+                img.setAttribute('src', 'imagens/adulto-homem.png')
+
+            }else {
+                img.setAttribute('src', 'imagens/idoso-homem.png')
+            }
+        }
+        
+        else {
+            genero = 'Feminino'
+            if(idade >=0 && idade <= 10) {
+                img.setAttribute('src', 'imagens/bebe-menina.png')
+
+            }else if (idade <= 21) {
+                img.setAttribute('src', 'imagens/jovem-mulher.png')
+
+            }else if (idade <= 50) {
+                img.setAttribute('src', 'imagens/adulto-mulher.png')
+
+            }else {
+                img.setAttribute('src', 'imagens/idoso-mulher.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        img.style.padding ='10px'
+        res.innerHTML = `Detectamos pessoa do sexo ${genero} com ${idade} anos.<br>`
+        res.appendChild(img)
+    }
     
 }
