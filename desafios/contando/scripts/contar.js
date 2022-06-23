a77d6269aefa258aa0ccab1484d217d8a0ad018e
Fabiano-braga <fabiano.braga@a-technologies.com.br>
2022-06-23 15:14:27 -0300

    Update contar.js

--- desafios/contando/scripts/contar.js
+++ desafios/contando/scripts/contar.js
@@ -6,17 +6,28 @@ function contar() {
 
     if (Ini.value.length == 0 || Fim.value.length == 0 || Passo.value.length == 0 ) {
         alert('ERRO: Verifique o formulário e tente novamente...')
+        res.innerHTML='ERRO!'
 
     }else {
         res.innerHTML = 'Contando...<br>'
-
         let inicio = Number(Ini.value)
         let fim = Number(Fim.value)
         let passo = Number(Passo.value)
 
-        
-        
+        if (passo == 0 ) {
+            alert(`Passo inválido! Vamos considerar o passo com o valor 1.`)
+            passo = 1
 
+        }if (inicio < fim) {
+            for (cont = inicio; cont <= fim; cont += passo) {
+            res.innerHTML +=` ${cont}, `
+
+        }
+        }else{
+            for (cont = inicio; cont >= fim; cont -= passo) {
+                res.innerHTML += `${cont}, `
+            }
+        }
+    res.innerHTML +='FIM!'
     }
-
 }
