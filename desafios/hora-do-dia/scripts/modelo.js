1ab9e10352d9c06d4668c7e1f440cf865060ebd5
Fabiano-braga <fabiano.braga@a-technologies.com.br>
2022-06-23 15:40:15 -0300

    Desafio finalizado.

--- desafios/hora-do-dia/scripts/modelo.js
+++ desafios/hora-do-dia/scripts/modelo.js
@@ -1,13 +1,13 @@
 function carregar() {
     let data = new Date()
-    let hora = data.getHours()
+    let hora = 20//data.getHours()
     let msg = document.getElementById('msg')
     let img = document.getElementById('imagem')
     msg.innerHTML = `Agora são ${hora} horas`
 
     if (hora >=0 && hora < 12) {
         img.src = 'imagens/manha.png'
-        document.body.style.background = '#375b73'
+        document.body.style.background = '#7ab9f5c9'
 
     }else if (hora >= 12 && hora < 18) {
         img.src = 'imagens/tarde.png'
