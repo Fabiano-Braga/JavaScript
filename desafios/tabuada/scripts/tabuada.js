fa1a06262896654a920584021428030e7af69856
Fabiano-braga <fabiano.braga@a-technologies.com.br>
2022-06-24 12:18:15 -0300

    Desafio finalizado.

--- desafios/tabuada/scripts/tabuada.js
+++ desafios/tabuada/scripts/tabuada.js
@@ -1,5 +1,24 @@
 function Tabuada() {
+    let num = document.getElementById('txtnum')
+    let select = document.getElementById('select')
+
+    if (num.value.length == 0) {
+        alert('ERRO: Campo "Número" está com o valor nulo.')
+
+    }else {
+        let n = Number(num.value)
+        let cont = 1
+        select.innerHTML = ''
+
+        while (cont <=10) {
+            let tabela = document.createElement('option')
+            tabela.text = `${n} x ${cont} = ${n*cont} `
+            select.appendChild(tabela)
+            cont++
+        }
+        
 
 
+    }
     
 }
