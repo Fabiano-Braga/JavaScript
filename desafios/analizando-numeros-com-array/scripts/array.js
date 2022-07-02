e3e44e8bb37e4f6e606dcc76120c4b7c61724216
Fabiano-braga <fabiano.braga@a-technologies.com.br>
2022-07-02 01:54:04 -0300

    Desafio em andamento

--- desafios/analizando-numeros-com-array/scripts/array.js
+++ desafios/analizando-numeros-com-array/scripts/array.js
@@ -3,3 +3,22 @@ let select = document.getElementById('tab')
 let res = document.getElementById('res')
 let valores = []
 
+function adicionar() {
+
+}
+
+function isNumero() {
+
+}
+
+function inLista() {
+
+}
+
+function finalizar() {
+
+}
+
+function zerar() {
+    
+}
